b563645f64cc51741320f6af08a555cdd9e44dab
GlobalEmanuel <global.emanuel@ao3tech.com>
2022-01-02 20:23:32 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EstruturaIfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a Nota do Aluno");
+            var notaDigitada = Console.ReadLine();
+
+            var nota = int.Parse(notaDigitada);
+
+            if (nota >= 70)
+            {
+                Console.WriteLine("Aprovado! Parabéns!");
+            }
+            else if (nota >= 40)
+            {
+                Console.WriteLine("Recuperação  Nota precisa ser maior que 70.");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado!");
+            }
+        }
+    }
+}
